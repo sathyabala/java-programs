ebb0593fa4aec44269c832dec6d75c74b42f3ddb
sathyabala <sathya.mrkit@gmail.com>
2016-09-14 17:06:20 +0530

    evenodd

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class EvenOdd
+{
+public static void main(String args[])
+{
+int a=10;
+if(a%2==0)
+{
+System.out.println("number is even");
+}
+else{
+System.out.println("number is odd");}}}
